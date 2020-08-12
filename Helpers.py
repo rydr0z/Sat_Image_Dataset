fb1bf40a5a111668f6989ed248229915bec15a02
rydr0 <56206508+rydr0@users.noreply.github.com>
2020-08-12 00:15:20 -0700

    Updated Training and Test data

--- Helpers.py
+++ Helpers.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
+from skimage import exposure
 
 
 
@@ -9,7 +10,8 @@ def clean_dataset(dataset):
     for i in range(0, dataset.n_images):
         NA_sum += torch.sum(torch.isnan(dataset.x[i]))
         dataset.x[i][torch.isnan(dataset.x[i])] = 0
-    print("NAN pixel values found and replaced:" + str(NA_sum.item()))
+    NA_percent = NA_sum.item() / ( dataset.n_images * dataset.x[0].shape[1] * dataset.x[0].shape[2])
+    print("NAN pixel values found and replaced:" + str(NA_sum.item()) + "\nPercentage of total pixels:" + str(NA_percent))
   
   
 def calculate_mean(dataset):
@@ -30,8 +32,9 @@ def calculate_std(dataset):
     std = torch.std(images, dim=(0,2,3), keepdim=True)
     return std
     
-def show_images(band, cmap, dataset):
-
+def show_images(band, cmap, dataset, seed):
+    
+    torch.manual_seed(seed)
     train_loader_for_vis = torch.utils.data.DataLoader(dataset,
                                                    batch_size=1,
                                                    shuffle=True)
@@ -39,8 +42,9 @@ def show_images(band, cmap, dataset):
     vis_images = []
     vis_labels = []
 
+    data_iter = iter(train_loader_for_vis)
     for i in range(1,6):
-        vis_image, vis_label = iter(train_loader_for_vis).next()
+        vis_image, vis_label = data_iter.next()
         vis_images.append(vis_image)
         vis_labels.append(vis_label)
 
@@ -49,17 +53,19 @@ def show_images(band, cmap, dataset):
     rows = 1
 
     ax = []
-    plt.rcParams.update({'font.size': 16})
+    plt.rcParams.update({'font.size': 18})
     for i in range(1, columns * rows + 1):
         image, label = vis_images[i-1], vis_labels[i-1]
         ax.append(fig.add_subplot(rows, columns, i))
-        ax[-1].set_title("Image {i} - Population: ".format(i=i) + str(label[:,1,:].item()))
+        ax[-1].set_title("Image {i}\nPopulation: {p}\nClass: {c}".format(i=i, p=str(label[:,1,:].item()), c=str(label[:,2,:].item())))
         if band != 5:
-            plt.imshow(image[0].permute(2, 1, 0)[:, :, band]/3000, cmap=cmap)
+            plt.imshow(image[0].permute(2, 1, 0)[:, :, band]/3000, cmap=cmap, vmin=0, vmax=1)
         else:
-            plt.imshow(image[0].permute(2, 1, 0)[:, :, band]/3000, cmap=cmap)
+            print(torch.max(image[0].permute(2, 1, 0)[:, :, band]))
+            plt.imshow(image[0].permute(2, 1, 0)[:, :, band], cmap=cmap)
     plt.show()
     plt.rcParams.update({'font.size': 12})
+    plt.savefig("\content\drive\My Drive\Dissertation Files\Exported Figures\Example Images - Band {}".format(band), format="png")
     
     
 def load_images_and_labels(dataset, num_images):
